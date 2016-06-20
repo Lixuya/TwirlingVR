00649c2c563e21962142b7b29332d374716e26e7
xieqiupeng <654104254@qq.com>
2016-06-20 14:57:44 +0800

    no message

--- app/src/com/twirlingvr/www/utils/constants.java
+++ app/src/com/twirlingvr/www/utils/constants.java
@@ -10,11 +10,10 @@ public class Constants {
     public static final String MIME_ZIP = "application/zip";
     //
     public static final String PATH_SERVER = "http://www.twirlingvr.com/";
-    public static final String PATH_DOWNLOAD = "content://downloads/my_downloads/";
-    public static final String PAPH_IMAGE = "http://www.twirlingvr.com/App_Media/images/";
-    public static final String PAPH_VIDEO = "http://www.twirlingvr.com/App_Media/videos/";
+    public static final String PAPH_IMAGE = PATH_SERVER + "App_Media/images/";
+    public static final String PAPH_VIDEO = PATH_SERVER + "App_Media/videos/";
     //
-    public static final Uri URI_DOWNLOAD = Uri.parse(PATH_DOWNLOAD);
+    public static final Uri URI_DOWNLOAD = Uri.parse("content://downloads/my_downloads/");
     public static final String URI_VIDEO = "file:///storage/emulated/0/Download/";
     public static final String URL_AAC = "http://twirlingvr.com/test/wxyz9.mp4";
 }
